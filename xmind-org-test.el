ec6a6ea24e04d659fd2bdabec55e3047b64b9359
Akira Komamura <akira.komamura@gmail.com>
2024-07-23 23:16:42 +0900

    test: Download the test file
    The file used to be downloaded during the CI workflow, but it should be
    universal.

--- xmind-org-test.el
+++ xmind-org-test.el
@@ -3,8 +3,27 @@
 (require 'buttercup)
 (require 'xmind-org)
 
+(defvar url-http-end-of-headers)
+
+(defconst xmind-org-test-url
+  "https://raw.githubusercontent.com/ivan-kleshnin/frontend-mindmaps/master/middle.xmind")
+
+(defconst xmind-org-test-file-name
+  "middle.xmind")
+
+(defun xmind-org-test-download-test-data ()
+  (unless (file-exists-p xmind-org-test-file-name)
+    (with-current-buffer (url-retrieve-synchronously xmind-org-test-url t t)
+      (when (bound-and-true-p url-http-end-of-headers)
+        (delete-region (point-min)
+                       (if (markerp url-http-end-of-headers)
+                           (marker-position url-http-end-of-headers)
+                         url-http-end-of-headers)))
+      (write-region (point-min) (point-max) xmind-org-test-file-name))))
+
 (describe "Parse a file"
   (describe "middle.xmind"
+    (xmind-org-test-download-test-data)
     (let ((buffer (create-file-buffer "sample.org")))
       (it "parses a file"
         (unwind-protect
