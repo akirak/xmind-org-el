89c6f129c27ac6ee31d07d2aa41c6e1e0cab1531
Akira Komamura <akira.komamura@gmail.com>
2024-07-23 23:39:02 +0900

    Revert "fix: Specify the mode in interactive"
    This reverts commit eae09199eee3b49907ad6511e51e455f130cf8ea.
    
    The second argument in `interactive` is only supported in Emacs 28 or later.
    For now, I won't use it.

--- xmind-org.el
+++ xmind-org.el
@@ -130,7 +130,8 @@ If COMMENT is non-nil, the node will have a comment heading."
 ;;;###autoload
 (defun xmind-org-insert-file (file)
   "Insert an XMind FILE into the current Org buffer."
-  (interactive "f" org-mode)
+  (interactive "f")
+  (cl-assert (derived-mode-p 'org-mode))
   (-> (xmind-org-parse-content (expand-file-name file))
       (xmind-org-root-node)
       (xmind-org-insert-node)))
